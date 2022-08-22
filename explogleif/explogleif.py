2db5d3907b50430017cdadf3cfaaf730339b727e
christian lajouanie <christianlajouanie@gmail.com>
2022-08-22 17:30:15 +0200

    update gleif_status function

--- explogleif/explogleif.py
+++ explogleif/explogleif.py
@@ -1,19 +1,47 @@
-import requests
+# explogleif.py
+## custom functions needed to explore gleif API
 
-def count_all_lei():
+import requests
+from explogleif.entity import Entity
+
+def latest_status(country=None, category=None, status=None):
     url = 'https://api.gleif.org/api/v1/lei-records'
     
     headers = {
         'Accept': 'application/vnd.api+json'
         }
     
-    payload={
-    'page[number]': 1,
-    'page[size]': 1 # one page per lei
-    }
+    payload = {
+        # Country code (2 letters)
+        'filter[entity.legalAddress.country]': country,
+        
+        # Entity category
+        # BRANCH, FUND, SOLE_PROPRIETOR, GENERAL, RESIDENT_GOVERNMENT_ENTITY, INTERNATIONAL_ORGANIZATION
+        'filter[entity.category]': category,
+        
+        # List of status
+        # ISSUED, LAPSED, ANNULLED, PENDING_TRANSFER, PENDING_ARCHIVAL, DUPLICATE, RETIRED, MERGED
+        'filter[registration.status]': status,
+        
+        # pagination
+        'page[number]': 1, #Must be at least 1.
+        'page[size]': 1 # Must be between 1 and 200.
+        }
     
     response = requests.request('GET',url,headers=headers, data=payload).json()
     
-    # since we have one page per lei, number of pages == number of lei
-    return response['meta']['pagination']['total']
+    lei_count = response['meta']['pagination']['total'] # pagination is 1 entity per page, so number of pages = number of entities
+    latest_entity = Entity(
+        name=  response['data'][0]['attributes']['entity']['legalName']['name'],
+        lei= response['data'][0]['attributes']['lei'],
+        city= response['data'][0]['attributes']['entity']['legalAddress']['city'],
+        country= response['data'][0]['attributes']['entity']['legalAddress']['country']
+    )
+    
+    answer = {
+        "lei_count": lei_count,
+        "latest_entity": latest_entity
+    }
+    
+    return answer
     
