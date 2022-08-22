9c31364c164b08cd8a79c12a4b6833d85aa944eb
christian lajouanie <christianlajouanie@gmail.com>
2022-08-22 17:28:40 +0200

    modified app layout

--- app.py
+++ app.py
@@ -9,32 +9,37 @@ from explogleif import explogleif
 # web page configuration
 st.set_page_config(
     page_title="EXPLO GLEIF - exploration of GLEIF API",
-    page_icon=":cl:", # Clear Button emoji
-    # layout="wide",
-    # initial_sidebar_state="auto"
+    page_icon=":cl:" # Clear Button emoji
     )
-
 
 """
 # EXPLOGLEIF  
 This app is meant to explore the API of the [Global Legal Entity Identifier Foundation](https://www.gleif.org).  
-The API documentation [can be found here](https://documenter.getpostman.com/view/7679680/SVYrrxuU?version=latest#quick-start-guide).
-## Check current status of the GLEIF database  
+The API documentation [can be found here](https://documenter.getpostman.com/view/7679680/SVYrrxuU?version=latest#quick-start-guide).  
+  
+  
+### Total number of LEIs in GLEIF database  
 """
 
 now = datetime.now()
+gleif_status = explogleif.latest_status()
+
+lei_count = gleif_status['lei_count']
+latest_entity = gleif_status["latest_entity"]
+
 
 f"""
-On {now.strftime("%Y-%m-%d %H:%M:%S")}, there are {explogleif.count_all_lei()} LEI in total.
+On {now.strftime("%Y-%m-%d %H:%M:%S")}, there are {lei_count} LEIs in total in the GLEIF database.  
 """
 
-# if st.button('number of LEIs'):
-#     st.write('there so many LEI, this is incredible !')
-#     st.write(now.strftime("%Y-%m-%d %H:%M:%S"))
-# else:
-    
-    
-    
+
+f"""  
+### Latest entity registered
+The latest entity registered is {latest_entity.name}.  
+It is located in {latest_entity.city}, {latest_entity.country}.  
+Its LEI is {latest_entity.lei}.
+"""
+
 """
 :cl:
 """
