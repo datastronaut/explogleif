56a7b7bb67f993451d6bd1a57896fa0fb6c0ea6d
christian lajouanie <christianlajouanie@gmail.com>
2022-08-22 17:25:05 +0200

    create Entity class

---
+++ explogleif/entity.py
@@ -0,0 +1,9 @@
+class Entity:
+    def __init__(self,name,lei,city,country):
+        self.name = name
+        self.lei = lei
+        self.city = city
+        self.country = country
+
+        
+        
