5bdfacb20223b84a5f434dfe11bd9d013d5b4203
christian lajouanie <christianlajouanie@gmail.com>
2022-08-31 17:41:23 +0200

    modify function search_entities to match modifications in Entity class

--- explogleif/explogleif.py
+++ explogleif/explogleif.py
@@ -54,17 +54,10 @@ def search_entities(
 
     response = requests.get(url, params=params).json()
 
-    entity_list = []
+    entities = []
 
     for json_entity in response["data"]:
-        new_entity = Entity(
-            name=json_entity["attributes"]["entity"]["legalName"]["name"],
-            lei=json_entity["id"],
-            city=json_entity["attributes"]["entity"]["legalAddress"]["city"],
-            country=json_entity["attributes"]["entity"]["legalAddress"]["country"],
-        )
-        entity_list.append(new_entity)
+        new_entity = Entity(json_data=json_entity)
+        entities.append(new_entity)
 
-    total_number_of_results = response["meta"]["pagination"]["total"]
-
-    return entity_list, total_number_of_results
+    return entities
