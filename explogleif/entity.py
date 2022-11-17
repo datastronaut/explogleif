35ab7e30409c37bcf92c155b71cf3094015cef91
christian lajouanie <christianlajouanie@gmail.com>
2022-11-17 13:18:09 +0100

    correction of functions 'get_direct_children' and 'get_direct_parent'

--- explogleif/entity.py
+++ explogleif/entity.py
@@ -19,20 +19,20 @@ class Entity:
         self.city = json_data["attributes"]["entity"]["legalAddress"]["city"]
         self.country = json_data["attributes"]["entity"]["legalAddress"]["country"]
 
-    def get_direct_parents(self, page_size=200):
+    def get_direct_parent(self, page_size=200):
         """
-        Return a list of objects of class Entity that are direct parents of self
+        Return an objects of class Entity that is direct parents of self
         """
-        parents_json = requests.get(
-            f"https://api.gleif.org/api/v1/lei-records?filter[owns]={self.lei}&page[size]={page_size}"
+        parent_json = requests.get(
+            f"https://api.gleif.org/api/v1/lei-records/{self.lei}/direct-parent"
         ).json()["data"]
-        return [Entity(json_data=parent_json) for parent_json in parents_json]
+        return Entity(json_data=parent_json)
 
     def get_direct_children(self, page_size=200):
         """
         Return a list of objects of class Entity that are direct children of self
         """
         children_json = requests.get(
-            f"https://api.gleif.org/api/v1/lei-records?filter[ownedBy]={self.lei}&page[size]={page_size}"
+            f"https://api.gleif.org/api/v1/lei-records/{self.lei}/direct-children"
         ).json()["data"]
         return [Entity(json_data=child_json) for child_json in children_json]
